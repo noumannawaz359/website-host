9a9a712cf1526e9daab98e77a745ce773fb23080
Nouman Gujjar <Nouman Gujjar@DESKTOP-84BSJNG>
2025-02-14 16:07:25 +0500

    make route

--- cd/src/App.js
+++ cd/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./component/login/login";
+import Register from "./component/register/register";
+import Home from "./component/home/home";
 
 function App() {
+
   return (
-    <div>Hello World</div>
-  )
+    <div>
+      <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+          </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
