524e16a34d2e7bedd55c655fbf1e30d66b6090a0
Nouman Gujjar <Nouman Gujjar@DESKTOP-84BSJNG>
2025-02-14 17:01:12 +0500

    hash

--- cd/src/App.js
+++ cd/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./component/login/login";
 import Register from "./component/register/register";
 import Home from "./component/home/home";
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <div>
-      <Router basename="website-host">
+      <Router>
           <Routes>
-            <Route exact path="/website-host" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
               <Route exact path="/login" element={<Login />} />
               <Route exact path="/register" element={<Register />} />
           </Routes>
